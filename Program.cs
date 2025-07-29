7f3ea57f9e330c27ecdc1729859a35e5382be916
Immanuel448 <emmanuelmendoza448@gmail.com>
2025-07-29 08:49:14 -0600

    Ignorar archivos temporales de Visual Studio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimuladorDescargasAsync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
